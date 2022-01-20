f7a84a4bda20ce9a6332ce7d9be91bd87ba7229a
Christian Parker <cparker@talloaks-environmental.com>
2022-01-20 13:10:05 -0800

    Fixed one sensor_node insert bug
    Also added the times to the rejects inserts

--- openaq_fastapi/openaq_fastapi/ingest/lcs_ingest_sensors.sql
+++ openaq_fastapi/openaq_fastapi/ingest/lcs_ingest_sensors.sql
@@ -9,7 +9,8 @@ AND
 ms_sensorsystems.ingest_id=sensor_systems.source_id
 ;
 
-INSERT INTO rejects (tbl,r) SELECT
+INSERT INTO rejects (t, tbl,r) SELECT
+    now(),
     'ms_sensorsystems',
     to_jsonb(ms_sensorsystems)
 FROM ms_sensorsystems WHERE sensor_systems_id IS NULL;
@@ -19,7 +20,8 @@ SET sensor_systems_id = ms_sensorsystems.sensor_systems_id
 FROM ms_sensorsystems WHERE
 ms_sensors.ingest_sensor_systems_id = ms_sensorsystems.ingest_id;
 
-INSERT INTO rejects (tbl,r) SELECT
+INSERT INTO rejects (t, tbl,r) SELECT
+    now(),
     'ms_sensors',
     to_jsonb(ms_sensors)
 FROM ms_sensors WHERE sensor_systems_id IS NULL;
@@ -64,14 +66,17 @@ UPDATE ms_sensors SET measurands_id = 1   WHERE measurands_id is null and ms_sen
 
 DELETE FROM ms_sensors WHERE ingest_id ~* 'purple' AND measurands_id is null AND measurand in ('um003','um050','um005');
 
-INSERT INTO rejects (tbl,r) SELECT
+INSERT INTO rejects (t, tbl,r) SELECT
+    now(),
     'ms_sensors no measurand',
     to_jsonb(ms_sensors)
 FROM ms_sensors WHERE measurands_id IS NULL;
 
 INSERT INTO sensors (source_id, sensor_systems_id, measurands_id, metadata)
 SELECT ingest_id, sensor_systems_id, measurands_id, metadata
-FROM ms_sensors where measurands_id is not null and sensor_systems_id is not null
+FROM ms_sensors
+WHERE measurands_id is not null
+AND sensor_systems_id is not null
 ON CONFLICT (sensor_systems_id, measurands_id, source_id) DO
 UPDATE SET
     metadata=sensors.metadata || EXCLUDED.metadata
