f09f3c0978805d48d058b6d1f3d2bbec20448031
Christian Parker <cparker@talloaks-environmental.com>
2023-05-11 14:00:36 -0700

    Fixed geog sorting error

--- openaq_fastapi/openaq_fastapi/routers/locations.py
+++ openaq_fastapi/openaq_fastapi/routers/locations.py
@@ -104,7 +104,7 @@ class Locations(
         if stm == "location":
             stm = "name"
         elif stm == "distance":
-            stm = "st_distance(st_makepoint(:lon,:lat)::geography, geog)"
+            stm = "st_distance(st_makepoint(:lon,:lat)::geography, geom::geography)"
         elif stm == "count":
             stm = "measurements"
 
