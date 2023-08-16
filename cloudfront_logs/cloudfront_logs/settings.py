dc2dbbdaff16105a0149751317f4b796303ece55
Russ Biggs <russbiggs@gmail.com>
2023-08-16 07:59:48 -0600

    update .env file environ variable (#203)

--- cloudfront_logs/cloudfront_logs/settings.py
+++ cloudfront_logs/cloudfront_logs/settings.py
@@ -2,12 +2,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from os import environ
 
 
+def get_env():
+    parent = Path(__file__).resolve().parent.parent
+    env_file = Path.joinpath(parent, environ.get("DOTENV", ".env"))
+    return env_file
+
+
 class Settings(BaseSettings):
     ENV: str = "staging"
     CF_LOGS_LOG_LEVEL: str = "INFO"
 
     model_config = SettingsConfigDict(
-        extra="ignore", env_file="../.env", env_file_encoding="utf-8"
+        extra="ignore",
+        env_file=get_env(),
+        env_file_encoding="utf-8",
     )
 
 
