b93e05a737d2be8e32a57c0f51a42d64fa23610f
Russ Biggs <russbiggs@gmail.com>
2023-08-22 08:07:43 -0600

    merge conflict

--- cdk/settings.py
+++ cdk/settings.py
@@ -22,11 +22,11 @@ class Settings(BaseSettings):
     ROLLUP_LAMBDA_MEMORY_SIZE: int = 1536
     LOG_LEVEL: str = "INFO"
     HOSTED_ZONE_ID: str | None = None
-    HOSTED_ZONE_NAME: Union[str, None] = None
-    WEB_ACL_ID: Union[str, None] = None
-    DOMAIN_NAME: Union[str, None] = None
-    LOG_BUCKET: Union[str, None] = None
-    CERTIFICATE_ARN: Union[str, None] = None
+    HOSTED_ZONE_NAME: str | None = None
+    WEB_ACL_ID: str | None = None
+    DOMAIN_NAME: str | None = None
+    LOG_BUCKET: str | None = None
+    CERTIFICATE_ARN: str | None = None
     API_LAMBDA_MEMORY_SIZE: int = 1536
     API_LAMBDA_TIMEOUT: int = 15  # lambda timeout in seconds
     CF_LOGS_LAMBDA_MEMORY_SIZE: int = 1792
