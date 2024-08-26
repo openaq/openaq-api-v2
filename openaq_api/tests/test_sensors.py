e2f1d6b7a0e7676fc80d27fa4db544622e9076de
Russ Biggs <russbiggs@gmail.com>
2024-08-26 13:56:40 -0600

    refactor sensors and measurements endpoints

--- openaq_api/tests/test_sensors.py
+++ openaq_api/tests/test_sensors.py
@@ -12,44 +12,44 @@ def client():
     with TestClient(app) as c:
         yield c
 
+
 # mock sensor and node
 sensor = 1
 node = 1
 
 urls = [
     ## v2
-    {"path": "/v2/averages?locations_id=:node","status": 200},
-    {"path": "/v2/locations/:node","status": 200},
-    {"path": "/v2/latest/:node","status": 200},
-    {"path": "/v2/measurements?location_id=:node","status": 200},
+    {"path": "/v2/averages?locations_id=:node", "status": 200},
+    {"path": "/v2/locations/:node", "status": 200},
+    {"path": "/v2/latest/:node", "status": 200},
+    {"path": "/v2/measurements?location_id=:node", "status": 200},
     # all of the following have an added where clause
     # and we just want to make sure the sql works
-    {"path": "/v2/cities?limit=1","status": 200},
-    {"path": "/v2/countries?limit=1","status": 200},
-    {"path": "/v2/sources?limit=1","status": 200},
-    {"path": "/v3/manufacturers?limit=1","status": 200},
-    {"path": "/v3/locations?limit=1","status": 200},
-    {"path": "/v3/licenses", "status": 200 },
-    {"path": "/v3/licenses/:node", "status": 200 },
+    {"path": "/v2/cities?limit=1", "status": 200},
+    {"path": "/v2/countries?limit=1", "status": 200},
+    {"path": "/v2/sources?limit=1", "status": 200},
+    {"path": "/v3/manufacturers?limit=1", "status": 200},
+    {"path": "/v3/locations?limit=1", "status": 200},
+    {"path": "/v3/licenses", "status": 200},
+    {"path": "/v3/licenses/:node", "status": 200},
     ## v3
-    {"path": "/v3/instruments/3","status": 200},
-    {"path": "/v3/locations/:node","status": 200}, # after
-    {"path": "/v3/locations/:node/measurements","status": 200}, # after
-    {"path": "/v3/sensors/:sensor/measurements","status": 200}, # after
-    {"path": "/v3/sensors/:sensor","status": 200}, # after
+    {"path": "/v3/instruments/3", "status": 200},
+    {"path": "/v3/locations/:node", "status": 200},  # after
+    {"path": "/v3/sensors/:sensor/measurements", "status": 200},  # after
+    {"path": "/v3/sensors/:sensor", "status": 200},  # after
 ]
 
 
 @pytest.mark.parametrize("url", urls)
 class TestUrls:
     def test_urls(self, client, url):
-        path = url.get('path')
-        path = path.replace(':sensor', str(sensor))
-        path = path.replace(':node', str(node))
+        path = url.get("path")
+        path = path.replace(":sensor", str(sensor))
+        path = path.replace(":node", str(node))
         response = client.get(path)
-        code = url.get('status')
+        code = url.get("status")
         if code == 404:
             data = json.loads(response.content)
-            assert len(data['results']) == 0
+            assert len(data["results"]) == 0
         else:
-            assert response.status_code == url.get('status')
+            assert response.status_code == url.get("status")
