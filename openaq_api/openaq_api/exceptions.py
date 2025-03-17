eb015c7cd1f714cebb9f61dfc6a782c9fc3058d4
zmoon <zmoon92@gmail.com>
2025-03-17 11:00:44 -0500

    Include rate limit headers in 429 exception

--- openaq_api/openaq_api/exceptions.py
+++ openaq_api/openaq_api/exceptions.py
@@ -12,3 +12,11 @@ TOO_MANY_REQUESTS = HTTPException(
     status_code=status.HTTP_429_TOO_MANY_REQUESTS,
     detail="Too many requests",
 )
+
+
+def too_many_requests_with_headers(headers):
+    return HTTPException(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        detail="Too many requests",
+        headers=headers,
+    )
