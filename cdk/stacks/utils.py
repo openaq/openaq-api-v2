0b5edae862e9d3b9f39e672b9272319bc9ad5f3f
Christian Parker <c.andrew.parker@gmail.com>
2025-04-07 12:51:41 -0700

    Cleaning up before merge

--- cdk/stacks/utils.py
+++ cdk/stacks/utils.py
@@ -1,6 +1,7 @@
 from os import environ
 from pathlib import Path
 import subprocess
+import platform
 import docker
 import shutil
 import os
@@ -27,26 +28,52 @@ def create_dependencies_layer(
 
     if not environ.get("SKIP_BUILD"):
         print(f'Building {layer_id} into {output_dir}')
-        ## migrate to the package/function directory to export and install
-        subprocess.run(
-            f"""
-             cd ../{function_name} && poetry export --only main -o requirements.txt --without-hashes && \
-             poetry run python -m pip install -qq -r requirements.txt \
-             -t {output_dir}/python && \
-             cd {output_dir} && \
-             find . -type f -name '*.pyc' | \
-             while read f; do n=$(echo $f | \
-             sed 's/__pycache__\///' | \
-             sed 's/.cpython-[2-3][0-9]+//'); \
-             cp $f $n; \
-             done \
-             && find . -type d -a -name '__pycache__' -print0 | xargs -0 rm -rf \
-             && find . -type d -a -name 'tests' -print0 | xargs -0 rm -rf \
-             && find . -type d -a -name '*.dist-info' -print0 | xargs -0 rm -rf \
-             && find . -type f -a -name '*.so' -print0 | xargs -0 strip --strip-unneeded
-             """,
-            shell=True,
+        if 'arm' in platform.uname().version.lower():
+            shutil.copy(requirements_file, f"./requirements.docker.txt")
+            client = docker.from_env()
+            print("starting docker image build...")
+            client.images.build(
+                path=str("."),
+                dockerfile="Dockerfile",
+                platform="linux/amd64",
+                tag="openaqapidependencies",
+                nocache=False,
             )
+            print("docker image built.")
+            print("running docker container.")
+            client.containers.run(
+                image="openaqapidependencies",
+                remove=True,
+                volumes=[f"{str(Path(__file__).resolve().parent.parent)}:/tmp/"],
+                user=0,
+            )
+            p = pathlib.Path(f"{output_dir}").resolve().absolute()
+            if not os.path.exists(p):
+                os.mkdir(p)
+            print("cleaning up")
+            shutil.move("./python", str(p))
+            os.remove(f"./requirements.docker.txt")
+        else:
+            ## migrate to the package/function directory to export and install
+            subprocess.run(
+                f"""
+                 cd ../{function_name} && poetry export --only main -o requirements.txt --without-hashes && \
+                 poetry run python -m pip install -qq -r requirements.txt \
+                 -t {output_dir}/python && \
+                 cd {output_dir} && \
+                 find . -type f -name '*.pyc' | \
+                 while read f; do n=$(echo $f | \
+                 sed 's/__pycache__\///' | \
+                 sed 's/.cpython-[2-3][0-9]+//'); \
+                 cp $f $n; \
+                 done \
+                 && find . -type d -a -name '__pycache__' -print0 | xargs -0 rm -rf \
+                 && find . -type d -a -name 'tests' -print0 | xargs -0 rm -rf \
+                 && find . -type d -a -name '*.dist-info' -print0 | xargs -0 rm -rf \
+                 && find . -type f -a -name '*.so' -print0 | xargs -0 strip --strip-unneeded
+                 """,
+                shell=True,
+                )
 
     layer_code = aws_lambda.Code.from_asset(output_dir)
 
