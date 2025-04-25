1cb0637b75f62caa3a742c45893ec9883e09dc8a
Christian Parker <c.andrew.parker@gmail.com>
2025-04-25 07:41:28 -0700

    Merging changes from zmoon

--- openaq_api/exceptions.py
+++ openaq_api/exceptions.py
@@ -8,7 +8,9 @@ NOT_AUTHENTICATED_EXCEPTION = HTTPException(
     detail="Invalid credentials",
 )
 
-TOO_MANY_REQUESTS = HTTPException(
-    status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-    detail="Too many requests",
-)
+def TOO_MANY_REQUESTS(headers=None):
+    return HTTPException(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        detail="Too many requests",
+        headers=headers,
+    )
